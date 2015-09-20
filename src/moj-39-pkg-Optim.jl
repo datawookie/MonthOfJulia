fade91455c8e134034bebd6d694fca8570731e61
Andrew B. Collier <andrew@exegetic.biz>
2015-09-20 07:44:21 +0200

    Added initial speed into calculations.

--- src/moj-39-pkg-Optim.jl
+++ src/moj-39-pkg-Optim.jl
@@ -8,32 +8,34 @@ using NLopt
 
 # Using MathProgBase interface which gives compatibility with other optimisation packages.
 
-count = 0 # keep track of # function evaluations
+count = 0
 
 # The objective function. The gradient is only required for gradient-based algorithms.
 #
 # We'll attempt to optimise the function
 #
-# x[2] / x[1] * sin(x[3]) * cos(x[3])
+#   x[2] / x[1] * sin(x[3]) * cos(x[3]) * x[4]^2
 #
 # which characterises the range efficiency of a Trebuchet with
 #
 #   x[1] = counterweight mass,
-#   x[2] = projectile mass and
-#   x[3] = initial angle of projectile.
+#   x[2] = projectile mass,
+#   x[3] = initial angle of projectile and
+#   x[4] = initial speed of projectile.
 #
 function range(x::Vector, grad::Vector)
     if length(grad) > 0
-        grad[1] = - sin(x[3]) * cos(x[3]) * x[2] / x[1]^2 
-        grad[2] = sin(x[3]) * cos(x[3]) / x[1]
-        grad[3] = (2 * cos(x[3])^2 - 1) * x[2] / x[1]
+        grad[1] = - sin(x[3]) * cos(x[3]) * x[2] / x[1]^2 * x[4]^2
+        grad[2] = sin(x[3]) * cos(x[3]) / x[1] * x[4]^2
+        grad[3] = (2 * cos(x[3])^2 - 1) * x[2] / x[1] * x[4]^2
+        grad[4] = 2 * sin(x[3]) * cos(x[3]) * x[2] / x[1] * x[4]
     end
 
     global count
     count::Int += 1
     println("f_$count($x)")
 
-    sin(x[3]) * cos(x[3]) * x[2] / x[1]
+    x[4]^2 * sin(x[3]) * cos(x[3]) * x[2] / x[1]
 end
 
 # The constraint function is interpreted as ... <= 0.
@@ -53,28 +55,52 @@ function mass_constraint(x::Vector, grad::Vector, a, b)
         grad[1] = b / x[2]
         grad[2] = - b * x[1] / x[2]^2
         grad[3] = 0
+        grad[4] = 0
     end
     a + b * x[1] / x[2]
 end
 
-opt = Opt(:LD_MMA, 3)                           # Algorithm and dimension of problem
+# The kinetic energy of the projectile cannot be more than the initial potential energy of the mass.
+#
+#   x[2] * x[4]^2 / 2 <= x[1] * g * h
+#
+# where g is gravitational acceleration and h is initial height of counterweight.
+#
+using PhysicalConstants
+#
+function energy_constraint(x::Vector, grad::Vector, h)
+    g = PhysicalConstants.MKS.GravAccel
+    if length(grad) > 0
+        grad[1] = - g * h
+        grad[2] = x[4]^2 / 2
+        grad[3] = 0
+        grad[4] = x[2] * x[4]
+    end
+    x[2] * x[4]^2 / 2 - x[1] * g * h
+end
+
+opt = Opt(:LD_MMA, 4)                           # Algorithm and dimension of problem
 ndims(opt)
 algorithm(opt)
 algorithm_name(opt)                             # Text description of algorithm
 
-lower_bounds!(opt, [10, 0.5, 0.])               # Lower bounds on optimisation parameters
-upper_bounds!(opt, [1000, 10, pi/2])            # Upper bounds on optimisation parameters
+lower_bounds!(opt, [10, 0.5, 0., 0.])           # Lower bounds on optimisation parameters
+upper_bounds!(opt, [1000, 10, pi/2, 1e5])       # Upper bounds on optimisation parameters
 
-xtol_rel!(opt, 1e-4)                            # Relative tolerance on optimisation parameters
+xtol_rel!(opt, 1e-6)                            # Relative tolerance on optimisation parameters
 
 max_objective!(opt, range)                      # Specify objective function
 
-# Apply the inequality constraints (using values for a and b mentioned above).
+# Apply the mass constraints (using values for a and b mentioned above).
 #
 inequality_constraint!(opt, (x, g) -> mass_constraint(x, g,    1, -1), 1e-8)
 inequality_constraint!(opt, (x, g) -> mass_constraint(x, g, -100,  1), 1e-8)
 
-initial = [20, 1, pi/8];                        # Initial guess
+# Apply energy constraint. Assuming counterweight dropped from height of 3m.
+#
+inequality_constraint!(opt, (x, g) -> energy_constraint(x, g, 5), 1e-8)
 
-(maxf,maxx,ret) = optimize(opt, initial)        # Perform optimisation
-println("got $minf at $minx after $count iterations (returned $ret)")
+initial = [20, 1, pi/8, 100];                   # Initial guess
+
+(maxf, maxx, ret) = optimize(opt, initial)      # Perform optimisation
+println("got $maxf at $maxx after $count iterations.")
