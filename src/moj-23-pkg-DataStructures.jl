c289cf9d3d6f02cb7f2b4faa1e435321c9e62fff
Andrew B. Collier <andrew@exegetic.biz>
2015-09-28 05:00:47 +0200

    Changes for day 23.

--- src/moj-23-pkg-DataStructures.jl
+++ src/moj-23-pkg-DataStructures.jl
@@ -17,45 +17,80 @@ using DataStructures
 # - Linked List
 # - Sorted Dict, Sorted Multi-Dict and Sorted Set
 
+# STACK ---------------------------------------------------------------------------------------------------------------
+
+# Stack() implements push!(), top() and pop!().
+
+stack = Stack(ASCIIString)
+#
+push!(stack, "First in.")
+for n in [2:4]; push!(stack, "... $n"); end
+push!(stack, "Last in.")
+#
+isempty(stack)
+length(stack)
+#
+top(stack)                          # Look at item on top of the stack
+#
+length(stack)
+pop!(stack)                         # Remove item from top of stack
+length(stack)
+
+# QUEUE ---------------------------------------------------------------------------------------------------------------
+
+# Queue() implements enqueue!(), front(), back() and dequeue().
+
+queue = Queue(Any);
+#
+# Items are always added to the back of the queue.
+#
+enqueue!(queue, "First in.")
+for n in [2:4]; enqueue!(queue, n); end
+enqueue!(queue, "Last in.")
+#
+front(queue)                        # Look at item at front of the queue
+back(queue)                         # Look at item at back of queue
+#
+# Items can only be removed from the front of the queue.
+#
+dequeue!(queue)
+
 # DEQUE ---------------------------------------------------------------------------------------------------------------
 
 # Julia's builtin Vector type provides the same functionality as Deque but the latter grows more efficiently.
 
 x = Deque{Int}()
 #
-push!(x, 4)				# Add to back
+# Items can be added to the front and back of a deque.
+#
+push!(x, 4)                         # Add to back
 push!(x, 7)
 push!(x, 9)
-unshift!(x, 1)				# Add to front
+unshift!(x, 1)                      # Add to front
 #
-isempty(x)
-length(x)
+# Items can be removed from front and back of a deque.
 #
-pop!(x)					# Remove from back
-shift!(x)				# Remoce from front
+pop!(x)                             # Remove from back
+shift!(x)                           # Remoce from front
 #
-front(x)				# Peek at item at front
-back(x)					# Peek at item at back
-
-# STACK ---------------------------------------------------------------------------------------------------------------
-
-# Stack() implements push!(), top() and pop!().
-
-# QUEUE ---------------------------------------------------------------------------------------------------------------
-
-# Queue() implements enqueue!(), front(), back() and dequeue().
+front(x)                            # Look at item at front
+back(x)                             # Look at item at back
 
 # COUNTER -------------------------------------------------------------------------------------------------------------
 
 cnt = counter(ASCIIString)
 
-push!(cnt, "dogs")			# Add 1 dog
-push!(cnt, "cats", 3)			# Add 3 cats
-push!(cnt, "cats", 2)			# Add another 2 cats
+push!(cnt, "dog")                   # Add 1 dog
+push!(cnt, "cat", 3)                # Add 3 cats
+push!(cnt, "cat")                   # Add another cat
+push!(cnt, "mouse", 5)              # Add 5 mice
 
-cnt["cats"]				# How many cats do we have now?
+cnt
 
-pop!(cnt, "dogs")
+pop!(cnt, "cat")
+
+cnt["cat"]                          # How many cats do we have now?
+cnt["mouse"]                        # But we still have a handful of mice.
 
 # TRIE ----------------------------------------------------------------------------------------------------------------
 
@@ -70,6 +105,11 @@ trie["rob"]   = 27
 trie["roger"] = 52
 
 keys(trie)
+haskey(trie, "roger")
+
+get(trie, "roger")
+get(trie, "robert")
+get(trie, "robert", nothing)
 
 # Alternative constructor.
 #
@@ -77,5 +117,10 @@ trie = Trie(["amy", "ann", "emma", "rob", "roger"], [56, 15, 30, 27, 52])
 
 # SORTED CONTAINERS ---------------------------------------------------------------------------------------------------
 
-# SortedDict, SortedMultiDict, or SortedSet implement a wide range of functionality. They will have to wait for another
-# time though!
+# SortedDict
+
+# SortedMultiDict
+
+# SortedSet
+
+# HEAPS ---------------------------------------------------------------------------------------------------------------
